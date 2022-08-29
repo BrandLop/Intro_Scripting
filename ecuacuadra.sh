d10c8e692911b5698d62e5b7ee37843e4558bc9b
BrandLop <brandon.lopezt@alumno.buap.mx>
2022-08-28 20:41:39 -0500

    Agregando script que resuelve ecuacion cuadratica

---
+++ ecuacuadra.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Programa que calcula las raices de una ecuacion cuadratica
+
+echo -e "Teniendo en cuenta ax²+bx+c\n"
+read -p "Ingresa el valor de a: " a
+read -p "Ingresa el valor de b: " b
+read -p "Ingresa el valor de c: " c
+
+d=$(((b*b)-4*a*c))
+
+if (( d < 0)); then
+  echo "No existen soluciones reales :("
+else
+  x1=`echo "(0-$b+sqrt($d))/(2*$a)" | bc`
+  x2=`echo "(0-$b-sqrt($d))/(2*$a)" | bc`
+  echo -e "\nResultados\nx1 = $x1 \nx2 = $x2"
+fi

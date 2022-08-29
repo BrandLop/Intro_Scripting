26183ea28f8ea7b1abdde8c5c6d3a3ed1d063f78
BrandLop <brandon.lopezt@alumno.buap.mx>
2022-08-28 20:59:46 -0500

    Modificacion para mostrar resultados con decimal

--- ecuacuadra.sh
+++ ecuacuadra.sh
@@ -7,11 +7,10 @@ read -p "Ingresa el valor de b: " b
 read -p "Ingresa el valor de c: " c
 
 d=$(((b*b)-4*a*c))
-
 if (( d < 0)); then
   echo "No existen soluciones reales :("
 else
-  x1=`echo "(0-$b+sqrt($d))/(2*$a)" | bc`
-  x2=`echo "(0-$b-sqrt($d))/(2*$a)" | bc`
+  x1=`echo "scale = 2 ; (0-$b+sqrt($d))/(2*$a)" | bc`
+  x2=`echo "scale = 2 ; (0-$b-sqrt($d))/(2*$a)" | bc`
   echo -e "\nResultados\nx1 = $x1 \nx2 = $x2"
 fi
